1c1cd311df3cfed1f6743ce1d1fc1dd228f4ab0f
guanapi <planeta.chompi18@gmail.com>
2026-07-24 07:46:28 +0200

    Adding flag -E

--- cat.py
+++ cat.py
@@ -1,8 +1,26 @@
 import sys
+import argparse
 
-def cat(file):
-    with open(file, 'r') as f:
-        print(f.read(), end='')
+def cat(file, show_ends):
+    try:
+        with open(file, 'r') as f:
+            content = f.read()
+
+            if show_ends:
+                content = content.replace('\n', '$\n')
+
+            print(content, end='')
+    except:
+        print(f'cat: {file}: No such file or directory')
 
 if __name__ == "__main__":
-    cat(sys.argv[1])
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-E', action='store_true')
+
+    parser.add_argument('file')
+
+    args= parser.parse_args()
+
+    cat(args.file, args.E)
