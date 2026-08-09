1a9e9e4aae6f74860ea39beb9ac120bda7c53482
guanapi <planeta.chompi18@gmail.com>
2026-08-09 16:55:11 +0200

    this is my more function

---
+++ my_more.py
@@ -0,0 +1,16 @@
+import argparse
+
+def my_more(filename):
+    line_per_page = 25
+    line_counter = 0
+    with open(filename) as file:
+        for line in file:
+            print(line.rstrip())
+            line_counter += 1
+
+            if line_counter == line_per_page:
+                input("--Press enter to continue reading--")
+                line_counter = 0
+
+if __name__ == "__main__":
+    my_more("/home/guanapi/.zshrc")
